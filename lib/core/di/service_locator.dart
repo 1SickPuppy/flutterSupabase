f6343f309ffa2af060f0e81940929c8f932f1e11
jesper <hamdererindeibatman@gmail.com>
2025-10-12 09:10:50 +0200

    4j

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,13 +1,9 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../security/secure_storage_service.dart';
-import '../../features/voice_input/domain/voice_input_service.dart';
 import '../../features/voice_input/data/voice_input_service_impl.dart';
-import '../../features/data_extraction/domain/data_extraction_service.dart';
 import '../../features/data_extraction/data/data_extraction_service_impl.dart';
-import '../../features/pdf_generation/domain/pdf_generation_service.dart';
 import '../../features/pdf_generation/data/pdf_generation_service_impl.dart';
-import '../../features/supabase_integration/domain/supabase_service.dart';
 import '../../features/supabase_integration/data/supabase_service_impl.dart';
 
 // Singleton instances
